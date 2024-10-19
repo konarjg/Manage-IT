ba6a2759ac1100188bd1a9c05574774156374f2c
Krzysztof <konarskikrzysztof1@gmail.com>
2024-10-19 13:23:11 +0200

    Added PrefixManager with queries for prefixes, added OnPost method to handle register forms, added static instance of DatabaseAccess

---
+++ Manage IT/Web/Security.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+public static class Security
+{
+    public static string HashText(string text, Encoding encoding)
+    {
+        var data = encoding.GetBytes(text);
+        string result = string.Empty;
+
+        using (var sha512 = SHA512.Create())
+        {
+            result = encoding.GetString(sha512.ComputeHash(data));
+        }
+
+        return result;
+    }
+}
