af018c51ed16c0c4c23b351b36c4af81c70cb45e
pelletsstarPL <54317957+pelletsstarPL@users.noreply.github.com>
2024-10-21 21:28:11 +0200

    Update UserManager.cs
    do I understand it or not lol?

--- Manage IT/Web/Database/UserManager.cs
+++ Manage IT/Web/Database/UserManager.cs
@@ -8,21 +8,27 @@ public class UserManager
 
     public static void Instantiate()
     {
-
+        Instance = new UserManager();
     }
 
     public bool RegisterUser(User user)
     {
+        user.Login, user.Password, user.Email, user.Prefix, user.Phone;
+        //INSERT INTO Users(Login,Password,Email,Prefix,PhoneNumber) VALUES...
+         var query = FormattableStringFactory.Create("INSERT INTO dbo.Users (Login,Password,Email,Prefix,PhoneNumber) VALUES ('{0}', '{1}','{2)',{3},{4})", Login, Password, Email, Prefix, PhoneNumber);
         return false;
     }
 
     public bool LoginUser(User user)
     {
+        //SELECT email,password FROM Users
+        //SELECT login,password FROM Users
         return false;
     }
 
     private bool UserExists(User user)
     {
+        //SELECT user FROM Users
         return false;
     }
 }
