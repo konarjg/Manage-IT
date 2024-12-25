e8feba82c68eec4b4bde7dc55db6307cce002977
TailsDoll-OS <73347093+TailsDoll-OS@users.noreply.github.com>
2024-12-25 03:40:23 +0100

    Added Settings Window

---
+++ Manage IT/Desktop/AdminPanelWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desktop
+{
+    /// <summary>
+    /// Interaction logic for AdminPanelWindow.xaml
+    /// </summary>
+    public partial class AdminPanelWindow : Window
+    {
+        public AdminPanelWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Manage IT/Desktop/obj/Debug/net6.0-windows/Desktop.AssemblyInfo.cs
+++ Manage IT/Desktop/obj/Debug/net6.0-windows/Desktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bc030b8947181e8d33a5fd4a4be2ae5260b9fd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce0e0637158a46b835f6e61a73f1a46138f4298f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
