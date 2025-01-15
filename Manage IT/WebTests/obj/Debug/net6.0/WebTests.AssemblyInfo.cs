d8b6daa757d3ef621a9d9af8da64221dc0c659f5
Alicja Fuks <fuksalicja21@gmail.com>
2025-01-15 19:38:01 +0100

    progress

--- Manage IT/WebTests/obj/Debug/net6.0/WebTests.AssemblyInfo.cs
+++ Manage IT/WebTests/obj/Debug/net6.0/WebTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c1030fad009d128e65cda545de762ec3b075560")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
