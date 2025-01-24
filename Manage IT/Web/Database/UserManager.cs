1b6b070ae60fa31d276545c3fd263c65dbf61786
Krzysztof <konarskikrzysztof1@gmail.com>
2025-01-24 18:55:04 +0100

    Progress

--- Manage IT/Web/Database/UserManager.cs
+++ Manage IT/Web/Database/UserManager.cs
@@ -8,6 +8,7 @@ using System.Windows.Markup;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Http;
 using System.Text.Json;
+using System.Xml.Linq;
 
 public class UserManager
 {
@@ -17,6 +18,32 @@ public class UserManager
     public static void Instantiate()
     {
         Instance = new UserManager();
+    }
+
+    public bool GetUserPermissions(long userId, long projectId, out UserPermissions permissions)
+    {
+        List<UserPermissions> records;
+        var query = FormattableStringFactory.Create($"SELECT * FROM dbo.UserPermissions WHERE UserId = {userId} AND ProjectId = {projectId}");
+
+        bool success = DatabaseAccess.Instance.ExecuteQuery(query, out records);
+
+        if (!success)
+        {
+            permissions = null;
+            return false;
+        }
+
+        permissions = records.FirstOrDefault();
+
+        return true;
+    }
+
+    public bool CreatePermissions(long managerId, long projectId)
+    {
+        List<UserPermissions> records;
+        FormattableString query = FormattableStringFactory.Create($"INSERT INTO dbo.UserPermissions (ProjectId, UserId, Editing, InvitingMembers, KickingMembers) VALUES({projectId}, {managerId}, 1, 1, 1");
+
+        return DatabaseAccess.Instance.ExecuteQuery(query, out records);
     }
 
     public bool GetAllUsers(out List<User> users)
